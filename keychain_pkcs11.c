7aee53faa77a6624e73b63e97b1150ae7fbbc28a
Ken Hornstein <kenh@cmf.nrl.navy.mil>
2018-07-05 13:18:08 -0400

    Skeleton module building infrastructure
    This commit includes the basics for building a shared module with
    libtool.

---
+++ keychain_pkcs11.c
@@ -0,0 +1,12 @@
+/*
+ * Our main driver for the keychain_pkcs11 module
+ */
+
+#include <CoreFoundation/CoreFoundation.h>
+#include <Security/Security.h>
+
+#include <stdio.h>
+#include <string.h>
+
+#include "mypkcs11.h"
+
